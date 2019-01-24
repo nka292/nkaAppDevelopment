0bd6ce371d0a8d415f5c0aed0d9f60a1169b622c
Nana Asubonteng <namankwah16@students.desu.edu@nanas-macbook-pro.local>
2019-01-23 16:00:10 -0800

    Initial Commit

---
+++ Tip Calculator 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator 1
+//
+//  Created by Nana Asubonteng on 1/23/19.
+//  Copyright © 2019 Nana Asubonteng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
